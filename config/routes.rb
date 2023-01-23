4eba60f6d4cfdae75e1a93fabb2421d55745db33
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-01-23 16:01:13 -0600

    Update routes 🛣️

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :users
+
+  root "users#index"
+
+  resources :users do
+    resources :posts
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end

331bfaf2d714d986b6a3653bc8cc73d80bb9bb5c
joker69216 <hamaraad883@gmail.com>
2023-02-04 17:44:15 +0300

    modify application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :bio, :photo, :email, :password, :password_confirmation)
+      u.permit(:name, :bio, :photo, :email, :password, :password_confirmation, roles: [])
     end
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :email, :bio, :photo, :password, :current_password)
